5c9009a7e0d04934f5e819c8b45b31b8a9a853d5
Abdul12majid <yisaabdulmajid@gmail.com>
2025-03-10 04:59:53 -0700

    display invoices

--- rely_invoice/views.py
+++ rely_invoice/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from .models import RelyInvoice, Status
 
 # Create your views here.
 def rely_invoice(request):
-	return HttpResponse("Welcome To Rely")
+    all_invoice = RelyInvoice.objects.all()
+    all_statuses = Status.objects.all()
+    context = {
+        "all_invoice": all_invoice,
+        "all_statuses": all_statuses,
+    }
+    return render(request, 'rely_inv.html', context)
